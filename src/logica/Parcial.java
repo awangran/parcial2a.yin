a2e6e6a9dc448c573a03b1bf2a35643d269c1426
awangran <ashleero.yin@gmail.com>
2025-11-05 08:23:26 -0500

    clase principal y parcial done

---
+++ src/logica/Parcial.java
@@ -0,0 +1,38 @@
+package logica;
+
+public class Parcial {
+	
+	public String resolverEcuacionSegundoOrden(int a, int b, int c) {
+		
+		String respuesta = "";
+
+		// Excepcion no deividir por 0
+		if (a == 0) {
+		    throw new IllegalArgumentException("a no puede ser 0");
+		}
+
+		// discriminante que puede ser negativo
+		Double disc = Math.pow(b, 2) - 4 * a * c;
+
+	
+		if (disc < 0) {
+		    throw new IllegalArgumentException("Las raíces son imaginarias porque el discriminante es negativo.");
+		}
+
+		// calculo soluciones
+		Double solucion1 = (-b + Math.sqrt(disc)) / (2 * a);
+		Double solucion2 = (-b - Math.sqrt(disc)) / (2 * a);
+
+		
+		if (Double.compare(solucion1, solucion2) == 0) {
+		    respuesta = "Solo hay una raiz: " + solucion1;
+		} else {
+		    respuesta = "Las raices son: " + solucion1 + " y " + solucion2;
+		}
+		
+		
+		return respuesta;
+		
+	}
+
+}
--- src/presentacion/Principal.java
+++ src/presentacion/Principal.java
@@ -1,10 +1,34 @@
 package presentacion;
 
+import java.util.Scanner;
+
+import logica.Parcial;
+
 public class Principal {
 	
 	public Principal() {
-	//xdxdx	
+		System.out.println("Parcial 2 Ashlee Yin ewe ");
+	    Scanner scanner = new Scanner(System.in);
+
+
+		
+		Parcial parcial = new Parcial();
+		
+		System.out.print("Ingresa a: ");
+	    int a = scanner.nextInt();
+	    
+	    System.out.print("Ingresa b: ");
+	    int b = scanner.nextInt();
+	    
+	    System.out.print("Ingresa c: ");
+	    int c = scanner.nextInt();
+		
+		
+	    System.out.print(parcial.resolverEcuacionSegundoOrden(a, b, c));
+		
 	};
+	
+	
 
 	
 	
